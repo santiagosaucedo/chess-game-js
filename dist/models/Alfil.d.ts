1ca6463240977a68e3c833a8cb8daf784aa0881c
santiagosaucedo <santiagosaucedo55@gmail.com>
2026-03-11 17:10:25 -0300

    GitIgnore configurado correctamente

--- dist/models/Alfil.d.ts
+++
@@ -1,7 +0,0 @@
-import { Pieza } from './Pieza.js';
-import { Color, Posicion } from './types.js';
-export declare class Alfil extends Pieza {
-    constructor(color: Color, posicionInicial: Posicion);
-    esMovimientoValido(nuevaPosicion: Posicion): boolean;
-}
-//# sourceMappingURL=Alfil.d.ts.map
--- dist/models/Peon.d.ts
+++
@@ -1,7 +0,0 @@
-import { Pieza } from './Pieza.js';
-import { Color, Posicion } from './types.js';
-export declare class Peon extends Pieza {
-    constructor(color: Color, posicionInicial: Posicion);
-    esMovimientoValido(nuevaPosicion: Posicion, esCaptura?: boolean): boolean;
-}
-//# sourceMappingURL=Peon.d.ts.map
